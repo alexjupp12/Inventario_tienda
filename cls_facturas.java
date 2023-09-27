2e13f027c5cf30d79c60ecde316b74d9a3f9e486
alex lemus <lemuscoloradoalexhumberto@gmail.com>
2023-09-27 07:33:11 -0500

    igual

--- cls_facturas.java
+++ cls_facturas.java
@@ -1,3 +1,20 @@
+package Inventario_tienda;
 public class cls_facturas {
-    
+    private String str_cod_producto;
+    private int int_cantidad;
+    private float flt_valorVenta;
+    private float flt_total;
+
+    public cls_facturas(String codigo, int cantidad, float valor_venta){
+        this.str_cod_producto = codigo;
+        this.int_cantidad = cantidad;
+        this.flt_valorVenta = valor_venta;
+        this.flt_total = valor_venta * cantidad;
+    }
+
+    //Getters
+    public String getCodigo() {return this.str_cod_producto;}
+    public int getCantidad(){return this.int_cantidad;}
+    public float getValorVenta(){return this.flt_valorVenta;}
+    public float getTotal(){return this.flt_total;}
 }
